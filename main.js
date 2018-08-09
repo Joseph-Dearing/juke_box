60af20029f8367ab5225945cf6a0cf3e8a006448
Joe Dearing <joewillgoo@yahoo.com>
2018-08-09 09:58:31 -0400

    jukebox

---
+++ main.js
@@ -0,0 +1,42 @@
+window.onload = function() {
+    document.getElementById('my_Audio').play();
+}
+
+
+
+function pauseAudio() {
+    document.getElementById('my_Audio').pause();
+}
+
+
+function playAudio() {
+    document.getElementById('my_Audio').play();
+}
+
+function pauseAudio() {
+    document.getElementById('my_Other').pause();
+}
+
+function playAudio() {
+    document.getElementById('my_Other').play();
+}
+// function onKeyDown(event) {
+//     switch (event.keyCode) {
+//         ['case keyCode 32'] //Case32 is the spacebar button in JavaScript language
+//         if (play) {
+//             my_Audio.pause();
+//             play = false;
+//         }  else {
+//             myAudio.play();
+//             play = true;
+//         }
+//         break;
+    
+//     } 
+//   return false;
+
+
+// }
+// window.addEventListener('keydown', onKeyDown, false);
+
+
